81ea834b0ed7d3808ce65de963342ba247aadc8e
BoukharAbdo <boukhar.abdelmalak.officiel@gmail.com>
2024-01-17 10:45:17 +0100

    TEST TWALAND

--- components/GlobalPage.js
+++ components/GlobalPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Home from './Home';
 import Trash from './Trash';
 import SettingsPage from './SettingsPage';
@@ -10,7 +11,16 @@ const Tab = createBottomTabNavigator();
 const GlobalPage = () => (
   <NavigationContainer independent={true}>
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen 
+  name="Home" 
+  component={Home} 
+  options={{
+    tabBarIcon: ({ color, size }) => (
+      <Icon name="rocket" type="font-awesome" size={size} color={color} />
+    ),
+  }}
+/>
+
       <Tab.Screen name="Classtring" component={Trash} />
       <Tab.Screen name="Settings" component={SettingsPage} />
     </Tab.Navigator>
